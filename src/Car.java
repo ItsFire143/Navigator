42b40e28ff366cf6bd8156fd2ab5e784593fbc8e
ItsFire143 <112923603+ItsFire143@users.noreply.github.com>
2022-10-19 20:39:52 +0300

    last

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Car {
+import java.util.Scanner;
+
+public class Car implements Runnable {
+    private int distance;
+    private int speed;
+    private int time = 0;
+    private int lastTime;
+    private int lastDistance;
+    private int averageSpeed;
+
+    @Override
+    public void run() {
+        Scanner takeDistance = new Scanner(System.in);
+        distance = takeDistance.nextInt();
+        distance = distance*1000;
+        int distanceInput = distance;
+
+        while (distance > 0) {
+            speed = (int) (Math.random()*110 + 1);
+            distance -= speed;
+            time++;
+            averageSpeed = distance / time;
+            lastTime = distance / speed;
+            lastDistance = distanceInput - distance;
+            if (distance < 0){
+                System.out.println("Приехали");
+                System.out.println("Средняя скорость: " + 0 + " м/с");
+                System.out.println("Оставшееся расстояние: " + 0 + " м");
+                System.out.println("Примерно осталось времени: " + 0 + " с");
+                System.out.println("Расстояние: " + 0);
+            } else {
+                System.out.println("Средняя скорость: " + averageSpeed + " м/с");
+                System.out.println("Оставшееся расстояние: " + lastDistance + " м");
+                System.out.println("Примерно осталось времени: " + lastTime + " с");
+                System.out.println("Расстояние: " + distance);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
